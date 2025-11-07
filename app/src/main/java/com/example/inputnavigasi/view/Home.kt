a8d35dd5a2a305be0c15bd6dcc4472ae296bfb9c
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-07 14:57:38 +0700

    Menambahkan layout

--- app/src/main/java/com/example/inputnavigasi/view/Home.kt
+++ app/src/main/java/com/example/inputnavigasi/view/Home.kt
@@ -1,10 +1,15 @@
 package com.example.inputnavigasi.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen(
     onStartClick: () -> Unit
 ){
-
+    Box(
+        Modifier.fillMaxSize()) {
+    }
 }
