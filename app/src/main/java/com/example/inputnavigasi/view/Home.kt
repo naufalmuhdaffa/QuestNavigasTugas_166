0b21b6f8ef194e590aa02f7dcb274b87aa898f19
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-07 15:01:35 +0700

    Menambahkan layout

--- app/src/main/java/com/example/inputnavigasi/view/Home.kt
+++ app/src/main/java/com/example/inputnavigasi/view/Home.kt
@@ -1,9 +1,13 @@
 package com.example.inputnavigasi.view
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(
@@ -11,5 +15,13 @@ fun HomeScreen(
 ){
     Box(
         Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+        }
     }
 }
